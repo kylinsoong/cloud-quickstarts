4db9782ad0ae61bc3734cf54fccd8fb293633fe7
kylin <kylinsoong.1214@gmail.com>
2024-12-23 16:43:46 +0800

    update changes

---
+++ ve/doubao/helloworld.py
@@ -0,0 +1,32 @@
+from volcenginesdkarkruntime import Ark
+
+client = Ark(
+    base_url="https://ark.cn-beijing.volces.com/api/v3",
+)
+
+# Non-streaming:
+print("----- standard request -----")
+completion = client.chat.completions.create(
+    model="ep-20241223161354-cdgdg",
+    messages = [
+        {"role": "system", "content": "你是豆包，是由字节跳动开发的 AI 人工智能助手"},
+        {"role": "user", "content": "目前排名前十的大模型有哪些？"},
+    ],
+)
+print(completion.choices[0].message.content)
+
+# Streaming:
+print("----- streaming request -----")
+stream = client.chat.completions.create(
+    model="ep-20241223161354-cdgdg",
+    messages = [
+        {"role": "system", "content": "你是豆包，是由字节跳动开发的 AI 人工智能助手"},
+        {"role": "user", "content": "目前排名前十的大模型有哪些？"},
+    ],
+    stream=True
+)
+for chunk in stream:
+    if not chunk.choices:
+        continue
+    print(chunk.choices[0].delta.content, end="")
+print()
